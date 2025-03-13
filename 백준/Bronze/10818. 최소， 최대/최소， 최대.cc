07a058d2cdd9f314899300b9503d634a62f5f897
Cwldb <157681771+Cwldb@users.noreply.github.com>
2025-03-13 23:14:19 +0900

    [Bronze III] Title: 최소, 최대, Time: 404 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -1,27 +1,24 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 int main()
 {
 	int a;
+	int b;
+
 	cin >> a;
-	int *arr = new int[a];
-
-
-	for (int i = 0; i < a; i++)
-	{
-		cin >> arr[i];
-	}
-
+	
 	int max = -1000001;
 	int min = 1000001;
 
 	for (int i = 0; i < a; i++)
 	{
-		if (arr[i] > max)
-			max = arr[i];
-		if (arr[i] < min)
-			min = arr[i];
+		cin >> b;
+		if (b > max)
+			max = b;
+		if (b < min)
+			min = b;
 	}
 
 	cout << min << " " << max;
