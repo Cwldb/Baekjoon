397d4414216cb51202265d52f2880c79e0f99680
Cwldb <157681771+Cwldb@users.noreply.github.com>
2025-03-14 20:58:17 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	int a;
+
+	cin >> a;
+
+	int b;
+	b = a / 4;
+
+	for (int i = 0; i < b; i++)
+		cout << "long ";
+
+	cout << "int";
+}
