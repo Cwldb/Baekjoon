acfff32c7b6697675890f6d0f110e6af12a508eb
Cwldb <157681771+Cwldb@users.noreply.github.com>
2025-03-16 23:30:10 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm>
+#include<ctime>
+using namespace std;
+
+int main()
+{
+	cout << "         ,r'\"7" << endl;;
+	cout << "r`-_   ,'  ,/" << endl;
+	cout << " \\. \". L_r'" << endl;
+	cout << "   `~\\/" << endl;
+	cout << "      |" << endl;
+	cout << "      |";
+
+}
