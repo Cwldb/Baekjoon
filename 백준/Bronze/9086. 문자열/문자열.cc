e18875070b7ff9511c29fa2bb58aa0c0a572d8fb
Cwldb <157681771+Cwldb@users.noreply.github.com>
2025-03-23 22:31:11 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		string s;
+		cin >> s;
+
+		cout << s[0] << s[s.length() - 1] << endl;
+	}
+}
