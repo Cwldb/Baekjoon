a3969faa1c20772d791506d02b83019e9c6fb7c0
Cwldb <157681771+Cwldb@users.noreply.github.com>
2025-04-16 21:29:40 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d, e;
+	cin >> a >> b >> c >> d >> e;
+
+	cout << a + b + c + d + e;
+}
