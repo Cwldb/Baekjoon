40061feb5d50634e0d18186bec1e8397d255ee49
Cwldb <157681771+Cwldb@users.noreply.github.com>
2025-04-17 23:28:19 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	cout << "       _.-;;-._" << endl;
+	cout << "'-..-'|   ||   |" << endl;
+	cout << "'-..-'|_.-;;-._|" << endl;
+	cout << "'-..-'|   ||   |" << endl;
+	cout << "'-..-'|_.-''-._|" << endl;
+}
