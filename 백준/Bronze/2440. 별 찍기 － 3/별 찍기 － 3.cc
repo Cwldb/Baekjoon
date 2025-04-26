3da384b913484adc33f3e1d8dcf572f6f7b23751
Cwldb <157681771+Cwldb@users.noreply.github.com>
2025-04-26 21:16:46 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() 
+{
+	int a;
+	cin >> a;
+
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = a; j > i; j--)
+		{
+			cout << "*";
+		}
+		cout << endl;	
+	}
+}
