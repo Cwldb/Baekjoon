5b2611f580e51698cfc65dbbc2bc1630370485d6
Cwldb <157681771+Cwldb@users.noreply.github.com>
2025-05-18 04:08:10 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int main() 
+{
+	string str;
+	cin >> str;
+
+	if(str == "SONGDO")
+		cout << "HIGHSCHOOL";
+	else if (str == "CODE")
+		cout << "MASTER";
+	else if (str == "2023")
+		cout << "0611";
+	else if (str == "ALGORITHM")
+		cout << "CONTEST";
+}
+
