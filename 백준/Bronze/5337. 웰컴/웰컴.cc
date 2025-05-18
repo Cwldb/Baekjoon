a21133f391a4f45cadd71cc2766ecc47ae352f08
Cwldb <157681771+Cwldb@users.noreply.github.com>
2025-05-18 23:25:57 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int main() 
+{
+	cout << ".  .   ." << '\n';
+	cout << "|  | _ | _. _ ._ _  _" << '\n';
+	cout << "|/\\|(/.|(_.(_)[ | )(/." << '\n';
+}
+
