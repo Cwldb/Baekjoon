fa2c9a5599aafb595fda42581908660490d3d5f2
Cwldb <157681771+Cwldb@users.noreply.github.com>
2025-06-18 23:11:22 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <list>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+int main() 
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cout << 2025 << '\n';
+	cout << 06 << '\n';
+	cout << 18 << '\n';
+}
+
