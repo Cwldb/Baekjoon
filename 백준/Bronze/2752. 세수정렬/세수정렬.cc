e6228693e4c617072e57c77821c692597f11ebe8
Cwldb <157681771+Cwldb@users.noreply.github.com>
2025-06-23 22:34:33 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <list>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+int main() 
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	vector<int> v(3);
+	for(int i = 0; i < 3; i++) 
+	{
+		cin >> v[i];
+	}
+
+	sort(v.begin(), v.end());
+	for (int i = 0; i < 3; i++) 
+	{
+		cout << v[i] << " ";
+	}
+	
+}
+
