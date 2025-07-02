c41a2ada818181957973b78bf7ef377135380fc3
Cwldb <157681771+Cwldb@users.noreply.github.com>
2025-07-02 21:20:05 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <list>
+#include <algorithm>
+#include <math.h>
+#include <map>
+
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a;
+    cin >> a;
+
+    if (a == 0)
+        cout << "YONSEI";
+    else
+        cout << "Leading the Way to the Future";
+
+}
+
