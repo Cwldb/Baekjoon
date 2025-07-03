03cb7400b53c4b4f6e2cc8ccb788ef8e15840bec
Cwldb <157681771+Cwldb@users.noreply.github.com>
2025-07-03 21:24:41 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <list>
+#include <algorithm>
+#include <math.h>
+#include <map>
+
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a;
+    cin >> a;
+
+    cout << a / 11 * 10 << '\n';
+
+}
+
