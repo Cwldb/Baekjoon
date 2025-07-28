d1a9e883808b7e72418700efcda1d1dd973f034c
Cwldb <157681771+Cwldb@users.noreply.github.com>
2025-07-28 22:32:09 +0900

    [Gold II] Title: 문제집, Time: 24 ms, Memory: 3944 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <list>
+#include <algorithm>
+#include <math.h>
+#include <map>
+#include <unordered_map>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, m;
+	cin >> n >> m;
+
+	vector<vector<int>> graph(n + 1);
+	vector<int> check(n + 1, 0);
+
+	priority_queue<int, vector<int>, greater<int>> q;
+
+	for (int i = 0; i < m; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+
+		graph[a].push_back(b);
+		check[b]++;
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (check[i] == 0)
+			q.push(i);
+	}
+
+	while (!q.empty())
+	{
+		int top = q.top();
+		q.pop();
+
+		cout << top << ' ';
+
+		for (auto next : graph[top])
+		{
+			check[next]--;
+			if (check[next] == 0)
+				q.push(next);
+		}
+	}
+}
