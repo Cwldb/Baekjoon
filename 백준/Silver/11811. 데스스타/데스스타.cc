6bd04fbda1ba8aff426b397d0f0899d816eca9ce
Cwldb <157681771+Cwldb@users.noreply.github.com>
2025-07-30 16:46:07 +0900

    [Silver III] Title: 데스스타, Time: 56 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11811. 데스스타/데스스타.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <list>
+#include <algorithm>
+#include <math.h>
+#include <map>
+#include <unordered_map>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		int a = 0;
+		for (int j = 0; j < n; j++)
+		{
+			int m;
+			cin >> m;
+
+			a |= m;
+		}
+
+		cout << a << " ";
+	}
+}
